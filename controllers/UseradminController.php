868c3704869e486ef3685c829e53f553ddac495e
Perunova Viktoria <cr_ovr_web10@efko.ru>
2024-10-01 16:31:24 +0300

    добавление примерной реализации формы управления пользователями

--- controllers/UseradminController.php
+++ controllers/UseradminController.php
@@ -3,12 +3,32 @@
 namespace app\controllers;
 
 use Yii;
+use app\models\User;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\web\ForbiddenHttpException;
 
 class UseradminController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        // Проверка на наличие прав администратора
+        if (Yii::$app->user->isGuest || Yii::$app->user->identity->getRole() !== 'admin') {
+            throw new ForbiddenHttpException('У вас нет доступа к этой странице.');
+        }
+
+        // Создаем провайдер данных для отображения пользователей в таблице
+        $dataProvider = new ActiveDataProvider([
+            'query' => User::find(),
+            'pagination' => [
+                'pageSize' => 10, // Можно настроить количество пользователей на странице
+            ],
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
     }
+
 }
--- views/useradmin/index.php
+++ views/useradmin/index.php
@@ -4,6 +4,7 @@ $this->title = 'Управление учетными записями';
 
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
+use yii\grid\GridView;
 ?>
 
 <div class="text-center mb-4 mt-3">
@@ -14,7 +15,40 @@ use yii\helpers\Html;
     <div class="col-md-6 border rounded-3 p-4 shadow">
         <?php $form = ActiveForm::begin(['id' => 'useradmin-form']); ?>
 
-        <p>В данный момент страница находится в разработке! Ожидайте...</p>
+
+
+<div class="user-index">
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            'id',
+            'username',
+            'email',
+            'role',
+            // Добавьте другие поля пользователя, если необходимо
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'header' => 'Действия',
+                'template' => '{update} {delete}',
+                'buttons' => [
+                    'update' => function ($url, $model) {
+                        return Html::a('Редактировать', $url, [
+                            'class' => 'btn btn-primary',
+                        ]);
+                    },
+                    'delete' => function ($url, $model) {
+                        return Html::a('Удалить', $url, [
+                            'class' => 'btn btn-danger',
+                            'data-method' => 'post',
+                            'data-confirm' => 'Вы уверены, что хотите удалить этого пользователя?',
+                        ]);
+                    },
+                ],
+            ],
+        ],
+    ]); ?>
+</div>
 
         <?php ActiveForm::end(); ?>
     </div>
